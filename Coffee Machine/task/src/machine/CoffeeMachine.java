ab688e2c521b61ab05a433e6f198ddbc89649d7f
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2020-11-26 22:25:13 +0200

    add Stage 3/6: Enough coffee for everyone

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -11,7 +11,7 @@ public class CoffeeMachine {
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");*/
-        System.out.println("Write how many cups of coffee you will need: ");
+        /*System.out.println("Write how many cups of coffee you will need: ");
         Scanner scanner = new Scanner(System.in);
         int cups = scanner.nextInt();
         System.out.println("For " + cups + " cups of coffee you will need:");
@@ -20,6 +20,37 @@ public class CoffeeMachine {
         int beans = cups * 15;
         System.out.println(water + " ml of water");
         System.out.println(milk + " ml of milk");
-        System.out.println(beans + " g of coffee beans");
+        System.out.println(beans + " g of coffee beans");*/
+        System.out.println("Write how many ml of water the coffee machine has:");
+        Scanner scanner = new Scanner(System.in);
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beans = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        int cupsNeed = scanner.nextInt();
+        int cupWater = water / 200;
+        int cupMilk = milk / 50;
+        int cupBeans = beans / 15;
+        int minCup = 0;
+        if (cupWater > cupMilk) {
+            minCup = cupMilk;
+        } else {
+            minCup = cupWater;
+        }
+
+        if (minCup > cupBeans) {
+            minCup = cupBeans;
+        }
+
+        if (minCup == cupsNeed) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (minCup < cupsNeed) {
+            System.out.println("No, I can make only " + minCup + " cup(s) of coffee");
+        } else {
+            minCup -= cupsNeed;
+            System.out.println("Yes, I can make that amount of coffee (and even " + minCup + " more than that)");
+        }
     }
 }
